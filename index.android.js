ed39283047ca06e35b857c7d815863f317549492
Daniel Ayala <dmayala001@gmail.com>
2016-06-07 16:12:33 -0700

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+import PluralTodo from './PluralTodo';
+AppRegistry.registerComponent('CrossTodo', () => PluralTodo);
